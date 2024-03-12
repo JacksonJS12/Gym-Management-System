372ef300323c963f004423fb8902e85e8c5260b6
JacksonJS12 <d.ivan0v.6100@gmail.com>
2024-03-12 17:26:46 +0200

    Initial commit.

---
+++ GymManagementSystem.Data.Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GymManagementSystem.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+
+    public class User : IdentityUser<Guid>
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ GymManagementSystem.Data/GymManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace GymManagementSystem.Data
+{
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class GymManagementDbContext : IdentityDbContext
+    {
+        public GymManagementDbContext(DbContextOptions<GymManagementDbContext> options) 
+            : base(options)
+        {
+            
+        }
+    }
+}
